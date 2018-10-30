6dd6460b087549e2a84956a73e32a4ad6e81e1bf
Dhruv Kumar <dhruvkmratiitk@gmail.com>
2018-10-31 03:27:34 +0530

    Added support for UserCreation

--- client.py
+++ client.py
@@ -1,21 +1,61 @@
+import json
 import datetime
 
 import sys
 import socket
 
-PORT = 24000
+SERVER_IP = '127.0.0.1' 
+PORT = 24001
+
+
+def setMessage(inp):
+    str1 = str(len(inp))
+    while(len(str1)<10):
+        str1 = "0" + str1
+    return str1.encode('utf-8') + inp
+
+def getMessage(s):
+    rawn = s.recv(10).decode('UTF-8')
+    if(len(rawn)==0):
+        return ""
+    print(rawn)
+    return s.recv(int(rawn)).decode('UTF-8')
 
 s = socket.socket()
-s.connect(('127.0.0.1', PORT))
-print("Enter movie title: ")
-inp = input()
+s.connect((SERVER_IP, PORT))
 
-s.send((inp).encode('UTF-8'))
-time1 = datetime.datetime.now()
+inp = ""
 
-rec = (s.recv(1024)).decode('UTF-8')
-time2 = datetime.datetime.now()
+while True:
+    print('Are you a new user - Y/N ?')
+    inp = input()
+    if inp.lower() not in ['y','n']:
+        print('Invalid Input...')
+        continue
+    break;
 
-print(rec)
-print("RoundTrip time "+str(time2-time1))
+print('Enter your username')
+username = input()
+
+if inp is 'Y' or inp is 'y':
+    print('good')
+else:
+    data = {'query':'UserExists?','value':username}
+    json_string = json.dumps(data)
+
+    s.sendall(setMessage((json_string).encode('UTF-8')))
+
+    #time1 = datetime.datetime.now()
+
+    rec = getMessage(s)
+    jrec = json.loads(rec)
+    print(jrec)
+    if jrec['code']==0:
+        print(jrec['response'])
+
+    print(rec)
+    time2 = datetime.datetime.now()
+
+#print(rec)
+#print("RoundTrip time "+str(time2-time1))
 s.close()
